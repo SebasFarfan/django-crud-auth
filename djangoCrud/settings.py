8ec8e64a48b8a0e154bdf3585c6b2d7d61827cda
Sebas <sebas.654@gmail.com>
2022-10-24 01:17:34 -0300

    arreglo en setting

--- djangoCrud/settings.py
+++ djangoCrud/settings.py
@@ -33,7 +33,7 @@ DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME: 
+if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
@@ -85,15 +85,10 @@ WSGI_APPLICATION = 'djangoCrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
-        dj_database_url.config(
-            default='postgresql://postgresql:postgresql@localhost/postgresql'
-        )
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
